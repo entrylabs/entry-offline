d827ff5597141db1820df2c16091b59f2fdb98c4
kimorkim <kimorkim@gmail.com>
2015-11-11 15:25:53 +0900

    Entry main source commit 1. package.json - npm dependence add 2. bower.son - bower dependence add

---
+++ entry_offline.js
@@ -0,0 +1,11 @@
+//var context = {};
+
+$(function () {
+	var workspace = document.getElementById("workspace");
+	var initOptions = {
+		type: 'workspace'
+	};
+	Entry.init(workspace, initOptions);
+	Entry.playground.setBlockMenu();
+	Entry.loadProject();
+});
