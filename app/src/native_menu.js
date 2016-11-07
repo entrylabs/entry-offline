b98b2214f2972db05237e96997ea3e3b70e387e5
jykim <kimorkim@gmail.com>
2016-11-07 14:18:53 +0900

    중간 커밋

--- app/src/native_menu.js
+++ app/src/native_menu.js
@@ -1,8 +1,7 @@
-$(document).on('mousewheel', function(e){
-    if(e.originalEvent.wheelDelta /120 > 0 && e.ctrlKey) {
+$(document).on('mousewheel', function(e) {
+    if (e.originalEvent.wheelDelta / 120 > 0 && e.ctrlKey) {
         Entry.plugin.setZoomInPage();
-    }
-    else if(e.ctrlKey){
+    } else if (e.ctrlKey) {
         Entry.plugin.setZoomOutPage();
     }
 });
@@ -18,21 +17,30 @@ var template = [{
     }, {
         label: Lang.Workspace.file_upload,
         accelerator: 'CmdOrCtrl+o',
-        click: function () {
+        click: function() {
             Entry.dispatchEvent('loadWorkspace');
+        }
+    }, {
+        type: 'separator'
+    }, {
+        label: '실과모드',
+        type: 'checkbox',
+        click: function(menuItem) {
+            localStorage.setItem('isMiniMode', menuItem.checked);
+            ipcRenderer.send('reload');
         }
     }, {
         type: 'separator'
     }, {
         label: Lang.Workspace.file_save,
         accelerator: 'CmdOrCtrl+s',
-        click: function () {
+        click: function() {
             Entry.dispatchEvent('saveWorkspace');
         }
     }, {
         label: Lang.Workspace.file_save_as,
         accelerator: 'Shift+CmdOrCtrl+S',
-        click: function () {
+        click: function() {
             Entry.dispatchEvent('saveAsWorkspace');
         }
     }]
@@ -61,19 +69,19 @@ var template = [{
     submenu: [{
         label: Lang.Menus.actual_size,
         accelerator: 'CmdOrCtrl+0',
-        click: function () {
+        click: function() {
             Entry.plugin.setZoomLevel(0);
         }
     }, {
         label: Lang.Menus.zoom_in,
         accelerator: 'CmdOrCtrl+=',
-        click: function () {
+        click: function() {
             Entry.plugin.setZoomInPage();
         }
     }, {
         label: Lang.Menus.zoom_out,
         accelerator: 'CmdOrCtrl+-',
-        click: function () {
+        click: function() {
             Entry.plugin.setZoomOutPage();
         }
     }]
@@ -103,39 +111,34 @@ if (process.platform == 'darwin') {
         }, {
             label: Lang.Menus.offline_quit,
             accelerator: 'Command+Q',
-            click: function() { 
-                console.log(app);
-                app.quit(); 
+            click: function() {
+                app.quit();
             }
         }]
     });
-    template[3].submenu.push(
-        {
-            type: 'separator'
-        }, {
-            label: 'Bring All to Front',
-            role: 'front'
-        }
-    );
+    template[3].submenu.push({
+        type: 'separator'
+    }, {
+        label: 'Bring All to Front',
+        role: 'front'
+    });
 } else {
-    template[0].submenu.push(
-        {
-            type: 'separator'
-        }, {
-            label: Lang.Menus.offline_quit,
-            accelerator: 'Alt+F4',
-            click: function () {
-                window.close();
-            }
+    template[0].submenu.push({
+        type: 'separator'
+    }, {
+        label: Lang.Menus.offline_quit,
+        accelerator: 'Alt+F4',
+        click: function() {
+            window.close();
         }
-    );
+    });
 
     template.push({
         label: Lang.Menus.help,
         submenu: [{
             label: Lang.Menus.entry_info,
             accelerator: 'F1',
-            click: function () {
+            click: function() {
                 Entry.plugin.openAboutPage();
             }
         }]
@@ -143,9 +146,8 @@ if (process.platform == 'darwin') {
 }
 
 var menu = Menu.buildFromTemplate(template);
-if(isOsx) {
+if (isOsx) {
     Menu.setApplicationMenu(menu);
 } else {
     BrowserWindow.fromWebContents(_webContents).setMenu(menu)
 }
-
--- app/src/workspace.js
+++ app/src/workspace.js
@@ -6,153 +6,20 @@ angular.module('workspace').controller("WorkspaceController", ['$scope', '$rootS
     $scope.isNowSaving = false;
     var supported = !(typeof storage == 'undefined' || typeof window.JSON == 'undefined');
     var storage = (typeof window.localStorage === 'undefined') ? undefined : window.localStorage;
+    var isMiniMode = false;
+    var defaultInitOption = {};
 
-    function a() {
-        EntryStatic.getAllBlocks = function() {
-            return [
-                {
-                    category: "start",
-                    blocks: [
-                        "when_run_button_click",
-                        "when_some_key_pressed",
-                        "mouse_clicked",
-                        "mouse_click_cancled",
-                        "when_object_click",
-                        "when_message_cast",
-                        "message_cast",
-                    ]
-                },
-                {
-                    category: "flow",
-                    blocks: [
-                        "wait_second",
-                        "repeat_basic",
-                        "repeat_inf",
-                        "repeat_while_true",
-                        "stop_repeat",
-                        "_if",
-                        "if_else",
-                        "wait_until_true",
-                    ]
-                },
-                {
-                    category: "moving",
-                    blocks: [
-                        "move_direction",
-                        "bounce_wall",
-                        "move_x",
-                        "move_y",
-                        "locate_y",
-                        "locate_xy",
-                        "locate_xy_time",
-                        "locate",
-                        "locate_object_time",
-                        "rotate_relative",
-                        "direction_relative",
-                        "rotate_absolute",
-                        "direction_absolute",
-                        "see_angle_object",
-                    ]
-                },
-                {
-                    category: "looks",
-                    blocks: [
-                        "show",
-                        "hide",
-                        "dialog_time",
-                        "change_to_some_shape",
-                        "change_to_next_shape",
-                        "add_effect_amount",
-                        "change_effect_amount",
-                        "erase_all_effects",
-                        "change_scale_size",
-                        "set_scale_size",
-                    ]
-                },
-                {
-                    category: "brush",
-                    blocks: [
-                        "brush_stamp",
-                        "start_drawing",
-                        "stop_drawing",
-                        "set_color",
-                        "change_thickness",
-                        "set_thickness",
-                        "brush_erase_all"
-                    ]
-                },
-                {
-                    category: "text",
-                    blocks: [
-                        "text_write",
-                        "text_append",
-                        "text_prepend",
-                        "text_flush"
-                    ]
-                },
-                {
-                    category: "sound",
-                    blocks: [
-                        "sound_something_with_block",
-                        "sound_something_wait_with_block",
-                        "sound_volume_change",
-                        "sound_volume_set",
-                    ]
-                },
-                {
-                    category: "judgement",
-                    blocks: [
-                        "is_clicked",
-                        "is_press_some_key",
-                        "reach_something",
-                        "boolean_basic_operator",
-                    ]
-                },
-                {
-                    category: "calc",
-                    blocks: [
-                        "calc_basic",
-                        "calc_rand",
-                        "coordinate_object",
-                        "quotient_and_mod",
-                        "get_project_timer_value",
-                        "choose_project_timer_action",
-                        "set_visible_project_timer",
-                        "length_of_string",
-                        "combine_something",
-                        "char_at",
-                        "substring",
-                        "replace_string",
-                    ]
-                },
-                {
-                    category: "variable",
-                    blocks: [
-                        "variableAddButton",
-                        "ask_and_wait",
-                        "get_canvas_input_value",
-                        "set_visible_answer",
-                        "get_variable",
-                        "change_variable",
-                        "set_variable",
-                        "show_variable",
-                        "hide_variable",
-                    ]
-                }, {
-                category: "arduino",
-                blocks: [
-                    "arduino_download_connector",
-                    "download_guide",
-                    "arduino_download_source",
-                    "arduino_connected",
-                    "arduino_reconnect",
-                ]
-            }]
-        }
+    function settingForMini() {
+        defaultInitOption = miniOption.initOptions;
+        EntryStatic.getAllBlocks = miniOption.allBlocks;
     }
 
     $scope.initWorkspace = function() {
-        a();
+        isMiniMode = localStorage.getItem('isMiniMode') == 'true';
+        if(isMiniMode) {
+            settingForMini();
+        }
+
         window.lang = localStorage.getItem('lang');
         Entry.addEventListener('showLoadingPopup', $scope.showLoadingPopup);
         Entry.addEventListener('hideLoadingPopup', $scope.hideLoadingPopup);
@@ -208,6 +75,7 @@ angular.module('workspace').controller("WorkspaceController", ['$scope', '$rootS
 
             initOptions.listEnable = false;
             initOptions.functionEnable = false;
+            initOptions.sceneEditable = false;
 
             Entry.init(workspace, initOptions);
 
@@ -787,8 +655,8 @@ angular.module('workspace').controller("WorkspaceController", ['$scope', '$rootS
                 object.sprite.pictures = [];
                 object.sprite.pictures.push({
                     dimension: {
-                        height: 1,
-                        width: 1
+                        height: 540,
+                        width: 960
                     },
                     filename: "_1x1",
                     name: Lang.Workspace.new_picture,
@@ -868,8 +736,8 @@ angular.module('workspace').controller("WorkspaceController", ['$scope', '$rootS
                 selectedItems.data.push({
                     fileurl: './bower_components/entryjs/images/_1x1.png',
                     dimension: {
-                        height: 1,
-                        width: 1
+                        height: 540,
+                        width: 960
                     },
                     filename: "_1x1",
                     name: Lang.Workspace.new_picture,
