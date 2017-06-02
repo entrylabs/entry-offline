f872dba95df66327543cd3c5effc0662c682b0cd
jykim <kimorkim@gmail.com>
2017-06-02 14:47:29 +0900

    오프라인 QA팀 이슈 처리

--- src/renderer/src/header.js
+++ src/renderer/src/header.js
@@ -165,10 +165,10 @@ angular.module('workspace').controller('HeaderController', ['$scope', '$rootScop
 
         $scope.showPythonTooltip = function() {
             new Entry.Tooltip([
-                {content:Lang.Workspace.textcoding_tooltip1, target: $(".workspaceModeSelector"), direction: "left", style: "edge_up"},
-                {content:Lang.Workspace.textcoding_tooltip2, target: $(".propertyTabconsole"), direction: "right"},
-                {content:Lang.Workspace.textcoding_tooltip3, target: $("#helpBtn"), direction: "down"}
-            ], {dimmed: true});
+                { content: Lang.Workspace.textcoding_tooltip1, target: $(".workspaceModeSelector"), direction: "left", style: "offline" }, 
+                { content: Lang.Workspace.textcoding_tooltip2, target: $(".propertyTabconsole"), direction: "right" }, 
+                { content: Lang.Workspace.textcoding_tooltip3, target: $("#helpBtn"), direction: "down" }
+            ], { dimmed: true });
         };
 
         $scope.dict = {
