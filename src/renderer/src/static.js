f4ad5f46b106dbf65b7204c589b1b46b0085c0d8
jykim <kimorkim@gmail.com>
2018-05-24 17:31:37 +0900

    update static.js

--- src/renderer/src/static.js
+++ src/renderer/src/static.js
@@ -1297,6 +1297,23 @@ EntryStatic.getAllBlocks = function() {
                 'funboard_dotmatrix_set',
                 'funboard_dotmatrix_1row',
                 'funboard_dotmatrix_1column',
+
+                //region microbit
+                "microbit_led_toggle",
+                "microbit_get_led",
+                "microbit_show_string",
+                "microbit_show_image",
+                "microbit_get_analog",
+                "microbit_get_analog_map",
+                "microbit_get_digital",
+                "microbit_get_button",
+                "microbit_get_sensor",
+                "microbit_get_accelerometer",
+                "microbit_play_note",
+                "microbit_change_bpm",
+                "microbit_set_bpm",
+                // "microbit_radio_receive_event",
+                //endregion microbit
             ],
         },
     ];
