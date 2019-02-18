8ec4d3f46ddece0317ecf5a83f31e86abe342451
extracold1209 <extracold1209@gmail.com>
2019-02-18 17:49:09 +0900

    webpack configuration 파일 분할

---
+++ webpack.main.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const setting = {
+    mode: 'none',
+    target: 'electron-main',
+    entry: './src/main.js',
+    resolve: {
+        extensions: ['.js', '.json'],
+    },
+    node: {
+        __dirname: false,
+        __filename: false,
+    },
+    output: {
+        path: path.resolve(__dirname, 'src', 'main_build'),
+        filename: '[name].bundle.js',
+        hotUpdateChunkFilename: 'hot/hot-update.js',
+        hotUpdateMainFilename: 'hot/hot-update.json',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|mjs)$/,
+                exclude: [
+                    /node_modules/,
+                ],
+                use: [{ loader: 'babel-loader' }],
+            },
+            {
+                test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|cur)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader',
+                options: {
+                    name: '[hash].[ext]',
+                    limit: 10000,
+                },
+            },
+        ],
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+};
+
+const NODE_ENV = process.env.NODE_ENV;
+
+if (NODE_ENV === 'production') {
+    setting.mode = 'production';
+} else if (NODE_ENV === 'development') {
+    setting.mode = 'development';
+    setting.output.devtoolLineToLine = true;
+    setting.devtool = 'source-map';
+}
+
+module.exports = setting;
---
+++ webpack.renderer.config.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const setting = {
+    mode: 'none',
+    target: 'electron-renderer',
+    entry: {
+        init: './src/renderer/initEntry.js',
+        render: './src/renderer/renderEntry.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'src', 'renderer_build'),
+        filename: '[name].bundle.js',
+        hotUpdateChunkFilename: 'hot/hot-update.js',
+        hotUpdateMainFilename: 'hot/hot-update.json',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx|mjs)$/,
+                exclude: [
+                    /node_modules/,
+                    /bower_components/,
+                    /modal[\\/]app\.js/,
+                    /entry-tool/,
+                ],
+                use: [{ loader: 'babel-loader' }],
+            },
+            {
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader', options: { importLoaders: 1 } },
+                        { loader: 'less-loader', options: { sourceMap: true } },
+                    ],
+                }),
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader', options: { importLoaders: 1, url: false } },
+                        { loader: 'sass-loader', options: { sourceMap: true } },
+                    ],
+                }),
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader',
+                }),
+            },
+            {
+                test: /\.(ico|png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot|cur)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader',
+                options: {
+                    name: '[hash].[ext]',
+                    limit: 10000,
+                },
+            },
+        ],
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css'),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+};
+
+const NODE_ENV = process.env.NODE_ENV;
+
+if (NODE_ENV === 'production') {
+    setting.mode = 'production';
+} else if (NODE_ENV === 'development') {
+    setting.mode = 'development';
+    setting.output.devtoolLineToLine = true;
+    setting.devtool = 'source-map';
+}
+
+
+module.exports = setting;
