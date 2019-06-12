93f0253c97b47c0a13a5efa68159cbccd5a865c6
extracold1209 <extracold1209@gmail.com>
2019-06-12 16:10:06 +0900

    aboutWindowManager ts화

--- src/main/views/aboutWindowManager.ts
+++ src/main/views/aboutWindowManager.ts
@@ -2,8 +2,12 @@ import { BrowserWindow } from 'electron';
 import path from 'path';
 
 export default class {
-    constructor(parentWindow) {
+    parentWindow: BrowserWindow;
+    aboutWindow?: BrowserWindow;
+
+    constructor(parentWindow: BrowserWindow) {
         this.parentWindow = parentWindow;
+        this.aboutWindow = undefined;
         this.createAboutWindow();
     }
     createAboutWindow() {
@@ -20,7 +24,7 @@ export default class {
         });
 
         this.aboutWindow.on('closed', () => {
-            this.aboutWindow = null;
+            this.aboutWindow = undefined;
         });
 
         this.aboutWindow.loadURL(`file:///${path.resolve(
@@ -32,9 +36,9 @@ export default class {
     openAboutWindow() {
         if (!this.aboutWindow) {
             this.createAboutWindow();
+        } else {
+            this.aboutWindow.show();
         }
-
-        this.aboutWindow.show();
     }
 
     closeAboutWindow() {
