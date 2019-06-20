3c9ed0642dd7cb1a79ce25c6b4c7d759509a0932
extracold1209 <extracold1209@gmail.com>
2019-06-20 15:21:15 +0900

    과거 reducer 삭제

--- src/renderer/reducers/index.js
+++ src/renderer/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import persistReducer from './persistReducer';
 import modalReducer from './modalReducer';
 import popupReducer from './popupReducer';
-import commonReducer from './commonReducer';
 
 // TODO ducks reducers
 import ducksPersistReducer from '../store/modules/persist';
