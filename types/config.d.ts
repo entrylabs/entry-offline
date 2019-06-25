c1fabbc960c3e503bf8926d7b69be559bbfe9f95
extracold1209 <extracold1209@gmail.com>
2019-06-25 19:32:52 +0900

    런타임 프로퍼티의 주석 추가

--- types/config.d.ts
+++ types/config.d.ts
@@ -22,10 +22,10 @@ declare type CommandLineOptions = CommandLineFlags & CommandLinePairs;
 
 // runtimeProperties
 declare type RuntimeGlobalProperties = {
-    roomIds: string[];
-    mainWindowId: number;
-    workingPath: string;
-    appName: string
+    roomIds: string[]; // cloud pc 용. 사용처 불분명. entry-hw 와 사용처 비교 필요
+    mainWindowId: number; // 메인 브라우저의 progressbar 컨트롤용
+    workingPath: string; // 프로젝트의 savePath 담당
+    appName: 'entry'; // 아직 렌더러 프로세스에서 실행하는 하드웨어 업데이트 로직 실행방지용
 }
 
 declare type GlobalConfigurations = CommandLineOptions & FileConfigurations & RuntimeGlobalProperties;
