2693007adc96fa86f9e475817781ae6e1991f1f6
extracold1209 <extracold1209@gmail.com>
2019-09-06 11:20:14 +0900

    [2.0.12] 교과형 하드웨어 카테고리 목록 개선된 형태로 수정

--- src/renderer/resources/static_mini.js
+++ src/renderer/resources/static_mini.js
@@ -1,4 +1,5 @@
 import { EntryStatic } from '../bower_components/entry-js/extern/util/static_mini.js';
+import _ from 'lodash';
 
 /**
  * entryjs 내 포함되어있는 EntryStatic 에 추가적인 코드를 덮어쓰기 하는 로직
@@ -134,6 +135,18 @@ EntryStatic.getQuestionCategoryData = function() {
         category: 'dummy',
         blocks: ['hidden_event', 'hidden', 'hidden_string', 'hidden_boolean'],
     };
+};
+
+const originGetAllBlocks = EntryStatic.getAllBlocks;
+EntryStatic.getAllBlocks = () => {
+    const allBlocks = originGetAllBlocks();
+    const arduino = _.find(allBlocks, ['category', 'arduino']);
+    arduino.blocks.forEach((block, index) => {
+        if (['arduino_download_connector'].indexOf(block) !== -1) {
+            arduino.blocks.splice(index, 1);
+        }
+    });
+    return allBlocks;
 };
 
 export default EntryStatic;
